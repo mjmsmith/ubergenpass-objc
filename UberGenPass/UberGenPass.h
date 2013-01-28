d7fa64ea0e036862ed1757bb882bc0b514b1297c
Mark Smith <mark@camazotz.com>
2013-01-28 10:29:00 -0500

    Oops.

---
+++ UberGenPass/UberGenPass.h
@@ -0,0 +1,13 @@
+//
+//  UberGenPass.h
+//  UberGenPass
+//
+//  Copyright (c) 2012 Camazotz Limited. All rights reserved.
+//
+
+#define AppVersionKey @"AppVersion"
+#define BackgroundTimeoutKey @"BackgroundTimeout"
+#define PasswordHashKey @"Hash"
+#define PasswordLengthKey @"PasswordLength"
+#define RecentSitesKey @"RecentSites"
+#define WelcomeShownKey @"WelcomeShown"
