62cc7d19e40f6d2f4a6eee3d4582bbc3a41d7b45
Mark Smith <mark@camazotz.com>
2013-01-28 22:58:26 -0500

    Layout changes.

--- UberGenPass/AboutViewController.m
+++ UberGenPass/AboutViewController.m
@@ -7,9 +7,14 @@
 
 
 #import "AboutViewController.h"
+#import "GradientButton.h"
+
+#define AppStoreReviewsURL @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=588224057"
 
 @interface AboutViewController ()
-@property (strong, readwrite, nonatomic) IBOutlet UIBarButtonItem *doneButtonItem;
+@property (strong, readwrite, nonatomic) IBOutlet UIImageView *iconImageView;
+@property (strong, readwrite, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, readwrite, nonatomic) IBOutlet GradientButton *rateButton;
 @property (strong, readwrite, nonatomic) IBOutlet UIWebView *webView;
 @end
 
@@ -23,19 +28,31 @@
 
 #pragma mark UIViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  self.nameLabel.text = [NSString stringWithFormat:@"%@ %@",
+                         [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleDisplayName"],
+                         [NSBundle.mainBundle objectForInfoDictionaryKey:@"CFBundleShortVersionString"]];
+  
+  self.iconImageView.layer.masksToBounds = YES;
+  self.iconImageView.layer.cornerRadius = 10;
+  
+  [self.rateButton useAlertStyle];
 }
 
 - (NSUInteger)supportedInterfaceOrientations {
-  return UIInterfaceOrientationMaskAllButUpsideDown;
+  return UIInterfaceOrientationMaskPortrait;
 }
 
 #pragma mark Actions
 
 - (IBAction)done {
   [self.delegate aboutViewControllerDidFinish:self];
+}
+
+- (IBAction)rate {
+  [[UIApplication sharedApplication] openURL:[NSURL URLWithString:AppStoreReviewsURL]];
 }
 
 @end
