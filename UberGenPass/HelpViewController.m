a08f47abf95ebfc4dde569f4641c832883103fb2
Mark Smith <mark@camazotz.com>
2013-02-08 17:52:01 -0500

    Auto layout.

--- UberGenPass/HelpViewController.m
+++ UberGenPass/HelpViewController.m
@@ -11,6 +11,8 @@
 @property (strong, readwrite, nonatomic) IBOutlet UIWebView *webView;
 @property (strong, readwrite, nonatomic) IBOutlet UIBarButtonItem *backButtonItem;
 @property (strong, readwrite, nonatomic) IBOutlet UIBarButtonItem *forwardButtonItem;
+@property (strong, readwrite, nonatomic) IBOutlet NSLayoutConstraint *navigationBarHeightConstraint;
+@property (strong, readwrite, nonatomic) IBOutlet NSLayoutConstraint *toolBarHeightConstraint;
 - (IBAction)done;
 - (IBAction)back;
 - (IBAction)forward;
@@ -35,8 +37,18 @@
   [self.webView loadRequest:[NSURLRequest requestWithURL:[NSBundle.mainBundle URLForResource:self.documentName withExtension:@"html"]]];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+  [self willRotateToInterfaceOrientation:UIApplication.sharedApplication.statusBarOrientation duration:0];
+  [super viewWillAppear:animated];
+}
+
 - (NSUInteger)supportedInterfaceOrientations {
   return UIInterfaceOrientationMaskAllButUpsideDown;
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+  self.navigationBarHeightConstraint.constant = UIInterfaceOrientationIsLandscape(toInterfaceOrientation) ? NavigationBarLandscapeHeight : NavigationBarPortraitHeight;
+  self.toolBarHeightConstraint.constant = UIInterfaceOrientationIsLandscape(toInterfaceOrientation) ? ToolBarLandscapeHeight : ToolBarPortraitHeight;
 }
 
 #pragma mark UIWebViewDelegate
